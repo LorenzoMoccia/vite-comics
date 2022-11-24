1d7da4a6b2d7ebf824481f238e58850703dcbd2a
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-24 18:17:14 +0100

    AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,6 +19,27 @@ export default {
             <ul class="icon-list">
                 <li>
                     <img src="../img/buy-comics-digital-comics.png" alt="DIGITAL">
+                    <p class="icon-text">DIGITAL COMICS</p>
+                </li>
+
+                <li>
+                    <img src="../img/buy-comics-merchandise.png" alt="DIGITAL">
+                    <p class="icon-text">DC MERCHANDISE</p>
+                </li>
+
+                <li>
+                    <img src="../img/buy-comics-subscriptions.png" alt="DIGITAL">
+                    <p class="icon-text">SUBSCRIPTION</p>
+                </li>
+
+                <li>
+                    <img src="../img/buy-comics-shop-locator.png" alt="DIGITAL">
+                    <p class="icon-text">COMIC SHOP LOCATOR</p>
+                </li>
+
+                <li>
+                    <img src="../img/buy-dc-power-visa.svg" alt="DIGITAL">
+                    <p class="icon-text">DC POWER VISA</p>
                 </li>
             </ul>
         </div>
@@ -35,12 +56,13 @@ export default {
 }
 .content-container{
   background-color: black;
+  padding: 20px;
   
 }
 
 .icon-container{
     background-color: #0282f9;
-
+    padding: 30px;
 }
 
 h1{
@@ -51,5 +73,26 @@ h1{
     list-style: none;
 }
 
+li{
+    display: flex;
+    align-items: center;
+    margin: auto;
+    padding: 10px;
+}
+
+.icon-text{
+    margin-left: 20px;
+    color: white;
+}
+
+ul{
+    display: flex;
+    
+}
+
+img{
+    width: 60px;
+    height: 55px;
+}
 
 </style>
