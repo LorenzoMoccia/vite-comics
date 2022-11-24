ecde016586de226d4b88720f7d7619d51b773d64
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-24 17:36:49 +0100

    AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,17 +13,42 @@ export default {
     <div class="container-logo">
       <img src="../img/dc-logo.png" alt="Logo">
     </div>
-    
-    <div class="container-navbar">
 
+    <div class="container-navbar">
+      <ul class="navbar-list">
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
     </div>
   </div>
   
 </template>
 
 <style lang="scss" scoped>
+.header-container{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
 
-
+li{
+  display: inline-block;
+  padding: 5px;
+}
+.container-navbar{
+  display: flex;
+}
+.navbar-list{
+  list-style: none;
+}
 
 
 </style>
