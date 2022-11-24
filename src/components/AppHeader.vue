c8b92d1430e3f9d0cb5da7a2ad6279a86a0e4fda
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-24 16:34:33 +0100

    AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+  
+  <div class="container">
+    <div>
+        Test
+    </div>
+    <div>
+        Test
+    </div>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+.container{
+    display: flex;
+    justify-content: space-between;
+}
+</style>
